4835c3b4da54f3554a1674b55ca54a0047f26a49
I s u R u  C h a M i k a <IsuruChamika007@gmail.com>
2024-11-09 09:52:57 +0530

    Uploaded by Isuru Chamika

---
+++ Database/isuruDataMsg.js
@@ -0,0 +1,8 @@
+{
+  "antibotQueenElisa": "\n```❌ Another Bot Message Detected :-``` *🤖 QueenElisa Md Bot ❗*\n",
+  "antibotSuhailMd": "\n```❌ Another Bot Message Detected :-``` *🤖 Suhail Md Bot ❗*\n",
+  "antibotQueenAmdi": "\n```❌ Another Bot Message Detected :-``` *🤖 QueenAmdi Md Bot ❗*\n",
+  "antibotRaganorkMd": "\n```❌ Another Bot Message Detected :-``` *🤖 Raganork Md Bot ❗*\n",
+  "antibotBaileysBot": "\n```❌ Another Bot Message Detected :-``` *🤖 Baileys based Bot ❗*\n",
+  "antibotZeroTwoBot": "\n```❌ Another Bot Message Detected :-``` *🤖 ZeroTwo Md Bot ❗*\n"
+}
